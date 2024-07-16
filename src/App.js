19159ab77245a2432c7ab209ef51a2898dd11eab
Safa Hadid <safahadid3@gmail.com>
2024-07-16 12:05:34 +0300

    Our first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import Employees from "./components/Employees";
+import "./App.css";
 
 function App() {
+  const showEmployees = false;
   return (
     <div className="App">
-      <p>App</p>
+      {showEmployees ? (
+        <>
+          <Employees />
+          <Employees />
+          <Employees />
+          <Employees />
+        </>
+      ) : (
+        <p>You can not see the employees</p>
+      )}
     </div>
   );
 }
---
+++ src/components/Employees.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Employees = () => {
+  return (
+    <div>Here is an employee</div>
+  )
+}
+
+export default Employees
